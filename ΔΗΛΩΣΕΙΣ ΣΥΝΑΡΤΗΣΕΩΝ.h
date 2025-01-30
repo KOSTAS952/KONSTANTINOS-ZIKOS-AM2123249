8093d283642e78d8efacad140559fdab57d77588
KOSTAS952 <kzikos777@gmail.com>
2025-01-30 17:14:15 +0200

    Update ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h

--- ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h
+++ ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h
@@ -1,1 +1,12 @@
+Δηλώσεις συναρτήσεων για SERVER.c
+
+void initialize_catalog();
+void process_order(int client_socket, char *client_name);
+void generate_report();
+void run_server();
+
+Δηλώσεις συναρτήσεων για CLIENT.c
+
+void run_client(int client_id);
+
 
